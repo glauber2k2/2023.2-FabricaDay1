c24fda6200e9c06204569bd1dd6c0d4bae3c84e2
glauber2k2 <devglaubermonteiro@gmail.com>
2023-08-30 18:18:01 -0300

    adicionando comentarios

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,33 @@
-'use client'
-import { useState } from 'react'
-import Pessoa from '@/components/Pessoa'
+'use client' //use client é obrigatorio quando for ter dados dinamicos na pagina.
+
+import { useState } from 'react' //Importação do Hook useState do React.
+import Pessoa from '@/components/Pessoa' //importação do componente Pessoa da pasta components.
 
 export default function Home() {
   const [verify, setVerify] = useState(true)
+  //para declarar um estado, que são como variaveis dinamicas no react, utilizamos o formato acima.
+
+  //dentro do [ ] colocamos separados por virgula, o nome da funcão de exibição e o nome da funcão de atualização.
+
+  //primeiro sempre vem a função de exibição, que no caso você escolhe o nome que vai dar ao estado, e após vem a de atualização que por padrão, e aconselhavel botar o mesmo nome, sendo que com um "set" no inicio.
+
+  //dentro dos ( ) do useState podemos colocar algum valor padrão que ja queremos que venha dentro da variavel.
 
   function trocarEstado() {
-    setVerify(!verify)
+    setVerify(!verify) // quando a variavel tem ! na frente, funciona para inverter seu valor. false -> true ou true -> false.
   }
 
   return (
-    <div>
+    <div className='p-8'>
       {verify && <Pessoa nome='Glauber' />}
-      <button onClick={trocarEstado}>Trocar</button>
+
+      <button
+        onClick={trocarEstado}
+        className='bg-green-200 text-black p-2 rounded mt-6'
+      >
+        {/* o OnClick serve basicamente para executar algo quando o botão for clicado, que no caso passamos a função trocarEstado para ser executada*/}
+        Trocar
+      </button>
     </div>
   )
 }
--- src/components/Pessoa.jsx
+++ src/components/Pessoa.jsx
@@ -1,3 +1,12 @@
-export default function Pessoa({nome}) {
-return <div>Nome: {nome}</div>
-}
+//Componente Pessoa.
+
+export default function Pessoa({nome}) { 
+    
+    //Basicamente, dentro da função Pessoa, podemos receber a propriedade name, que temos q colocar entre { } para conseguimos passar algum valor la no page, para ser executado no componente filho que no caso é o Pessoa.
+
+    return <div className="bg-zinc-500 p-4 rounded font-bold">Nome: {nome}</div> 
+} 
+
+//para usarmos o nome passado no componente lá na pagina principal, temos que colocar dentro do nossa renderização, dentro do return o nome da propriedade entre { }
+
+//so podemos retornar 1 elemento dentro do return, ou seja, o aconselhavel é retornar uma div ou um fragment <> </>, e colocar tudo dentro desse retorno principal seja div ou fragment.
