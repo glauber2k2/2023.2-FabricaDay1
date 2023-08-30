86d4fd5766fafc971320cdc31a4464f3f1d31a7d
glauber2k2 <devglaubermonteiro@gmail.com>
2023-08-30 18:21:51 -0300

    adicionando comentarios

--- src/components/Pessoa.jsx
+++ src/components/Pessoa.jsx
@@ -10,3 +10,11 @@ export default function Pessoa({nome}) {
 //para usarmos o nome passado no componente lá na pagina principal, temos que colocar dentro do nossa renderização, dentro do return o nome da propriedade entre { }
 
 //so podemos retornar 1 elemento dentro do return, ou seja, o aconselhavel é retornar uma div ou um fragment <> </>, e colocar tudo dentro desse retorno principal seja div ou fragment.
+
+//dentro do elemento jsx que são os elementos do html, como div, h1, h2, e etc... podemos colocar a estilização do elemento, através do tailwind que vem por padrão no next.
+
+//para adicionarmos a estilização temos que colocar dentro do elemento, um className="", e dentro das aspas iremos colocar toda nossa regra de estilização.
+
+//os elementos tem o nome bastante parecidos com o do css convencional, sendo que abreviados, para facilitar recomendo que instale o tailwind intellisense, que ao apertar ctrl + space, ele vai ta sugerindo o nome.
+
+//ou pode olhar também na documentação do tailwind: https://tailwindcss.com/
